95bffc90c7f222d7a2aef6ed595f4512076c676e
NoobKid04 <noobkid2004@gmail.com>
2025-05-19 10:14:53 +0700

    Merge branc 1eff284

--- can_pal_s32k144/Debug_FLASH/src/main.d
+++ can_pal_s32k144/Debug_FLASH/src/main.d
@@ -1,67 +1,65 @@
 src/main.o: ../src/main.c \
  E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/sdk_project_config.h \
  E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/clock_config.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/clock.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/device_registers.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/common/s32_core_cm4.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144_features.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/devassert.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/status.h \
- c:\nxp\s32ds.3.5\s32ds\software\s32sdk_s32k1xx_rtm_4.0.1\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
+ ../SDK/platform/drivers/inc/clock.h \
+ ../SDK/platform/devices/device_registers.h \
+ ../SDK/platform/devices/common/s32_core_cm4.h \
+ ../SDK/platform/devices/S32K144/include/S32K144.h \
+ ../SDK/platform/devices/S32K144/include/S32K144_features.h \
+ ../SDK/platform/devices/devassert.h ../SDK/platform/devices/status.h \
+ ../SDK/platform/drivers/inc/../src/clock/S32K1xx/clock_S32K1xx.h \
  E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/pin_mux.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/pins_driver.h \
+ ../SDK/platform/drivers/inc/pins_driver.h \
  E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/peripherals_can_pal1.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/pal/inc/can_pal.h \
+ ../SDK/platform/pal/inc/can_pal.h \
  E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/can_pal_cfg.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/pal/inc/can_pal_mapping.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/flexcan_driver.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/rtos/osif/osif.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/edma_driver.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/callbacks.h \
+ ../SDK/platform/pal/inc/can_pal_mapping.h \
+ ../SDK/platform/drivers/inc/flexcan_driver.h ../SDK/rtos/osif/osif.h \
+ ../SDK/platform/drivers/inc/edma_driver.h \
+ ../SDK/platform/devices/callbacks.h \
  E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/peripherals_osif1.h \
- C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/interrupt_manager.h
+ ../SDK/platform/drivers/inc/interrupt_manager.h
 
 E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/sdk_project_config.h:
 
 E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/clock_config.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/clock.h:
+../SDK/platform/drivers/inc/clock.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/device_registers.h:
+../SDK/platform/devices/device_registers.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/common/s32_core_cm4.h:
+../SDK/platform/devices/common/s32_core_cm4.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144.h:
+../SDK/platform/devices/S32K144/include/S32K144.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144_features.h:
+../SDK/platform/devices/S32K144/include/S32K144_features.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/devassert.h:
+../SDK/platform/devices/devassert.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/status.h:
+../SDK/platform/devices/status.h:
 
-c:\nxp\s32ds.3.5\s32ds\software\s32sdk_s32k1xx_rtm_4.0.1\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
+../SDK/platform/drivers/inc/../src/clock/S32K1xx/clock_S32K1xx.h:
 
 E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/pin_mux.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/pins_driver.h:
+../SDK/platform/drivers/inc/pins_driver.h:
 
 E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/peripherals_can_pal1.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/pal/inc/can_pal.h:
+../SDK/platform/pal/inc/can_pal.h:
 
 E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/can_pal_cfg.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/pal/inc/can_pal_mapping.h:
+../SDK/platform/pal/inc/can_pal_mapping.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/flexcan_driver.h:
+../SDK/platform/drivers/inc/flexcan_driver.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/rtos/osif/osif.h:
+../SDK/rtos/osif/osif.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/edma_driver.h:
+../SDK/platform/drivers/inc/edma_driver.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/callbacks.h:
+../SDK/platform/devices/callbacks.h:
 
 E:/SM32/CAN_Protocol_Project/can_pal_s32k144/board/peripherals_osif1.h:
 
-C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/interrupt_manager.h:
+../SDK/platform/drivers/inc/interrupt_manager.h:
