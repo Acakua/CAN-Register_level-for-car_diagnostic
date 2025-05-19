1ac1b4575e33da0fbe6d25e83727598851d4e488
phamngocdo <ngocdo992k4@gmail.com>
2025-05-19 11:31:43 +0700

    Add gitignore

--- can_pal_s32k144/Debug_FLASH/board/peripherals_osif1.d
+++
@@ -1,9 +0,0 @@
-board/peripherals_osif1.o: ../board/peripherals_osif1.c \
- ../board/peripherals_osif1.h ../SDK/rtos/osif/osif.h \
- ../SDK/platform/devices/status.h
-
-../board/peripherals_osif1.h:
-
-../SDK/rtos/osif/osif.h:
-
-../SDK/platform/devices/status.h:
--- can_pal_s32k144/Debug_FLASH/src/Send_Can.d
+++
@@ -1,4 +0,0 @@
-src/Send_Can.o: ../src/Send_Can.c \
- C:/Users/Admin/workspaceS32DS.3.5/CAN_Protocol_Project/can_pal_s32k144/src/Send_Can.h
-
-C:/Users/Admin/workspaceS32DS.3.5/CAN_Protocol_Project/can_pal_s32k144/src/Send_Can.h:
