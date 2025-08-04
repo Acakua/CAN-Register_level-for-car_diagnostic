f066012c217d92c7447ce9765efeb30a1713142b
Bùi Minh Huy <minhhuybui044@gmail.com>
2025-08-04 22:48:52 +0700

    Update FlexCan header

--- can_pal_s32k144/inc/FlexCan.h
+++ can_pal_s32k144/inc/FlexCan.h
@@ -1,10 +1,11 @@
 #ifndef FLEXCAN_H
 #define FLEXCAN_H
 
+
 #define RX_MB_INDEX  0UL
 #define TX_MB_INDEX  1UL
-#define RX_MSG_ID    0x769
-#define TX_MSG_ID    0x768
+#define RX_MSG_ID_UDS    0x769
+#define TX_MSG_ID_UDS    0x768
 #define MSG_BUF_SIZE 4
 
 typedef struct {
@@ -15,6 +16,7 @@ typedef struct {
 
 void FLEXCAN0_init(void);
 void FLEXCAN0_transmit_msg(const CAN_Message_t *msg);
-int FLEXCAN0_receive_msg(CAN_Message_t *msg);
+int FLEXCAN0_receive_msg(CAN_Message_t *msg, uint32_t expectedID);
+
 
 #endif
